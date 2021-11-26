0f0ad303ebe648795a8a7e22020603b0553aa99f
matheusnogueirasilva <matheuskynyrd55@gmail.com>
2021-11-26 19:21:03 -0300

    Aula Snapshot

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'contact_form.dart';
 
 class ContactsList extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,16 +12,38 @@ class ContactsList extends StatelessWidget {
         title: Text('Contacts'),
       ),
       body: FutureBuilder<List<Contact>>(
-        future: findAll(),
+        initialData: [],
+        future:findAll(),
         builder: (context, snapshot) {
-          final List<Contact>? contacts = snapshot.data;
-          return ListView.builder(
-            itemBuilder: (context, index) {
-              final Contact contact = contacts![index];
-              return _ContactItem(contact);
-            },
-            itemCount: contacts?.length,
-          );
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(),
+                    Text('Loading'),
+                  ],
+                ),
+              );
+              break;
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              final List<Contact>? contacts = snapshot.data;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final Contact contact = contacts![index];
+                  return _ContactItem(contact);
+                },
+                itemCount: contacts?.length,
+              );
+              break;
+          }
+          return Text('Unknown error');
         },
       ),
       floatingActionButton: FloatingActionButton(
